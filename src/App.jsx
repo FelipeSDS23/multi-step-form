32f0145b44258b37daad6699eac9d2cb8a2be8e5
FelipeSDS23 <felipe03san@gmail.com>
2023-09-14 17:39:22 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     <main className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path='https://felipesds23.github.io/multi-step-form/' element={<Step1 />} />
+          <Route path='/' element={<Step1 />} />
           <Route path='/step2' element={<Step2 />} />
           <Route path='/step3' element={<Step3 />} />
           <Route path='/step4' element={<Step4 />} />
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,45 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+
 import { PlanContextProvider } from './context/PlanContext.jsx';
+
+import Step1 from './pages/Step1';
+import Step2 from './pages/Step2';
+import Step3 from './pages/Step3';
+import Step4 from './pages/Step4';
+import Thanks from './pages/Thanks';
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Step1 />
+  },
+  {
+    path: "/step2",
+    element: <Step2 />
+  },
+  {
+    path: "/step3",
+    element: <Step3 />
+  },
+  {
+    path: "/step4",
+    element: <Step4 />
+  },
+  {
+    path: "/thanks",
+    element: <Thanks />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PlanContextProvider>
-      <App />
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
     </PlanContextProvider>
   </React.StrictMode>,
 )
