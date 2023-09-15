2126525155c72cdb8ba14f9ee2f3e47b0e132733
FelipeSDS23 <felipe03san@gmail.com>
2023-09-14 22:02:20 -0300

    teste pages

--- src/pages/Step1.jsx
+++ src/pages/Step1.jsx
@@ -62,52 +62,59 @@ const Step1 = () => {
   // FIM DAS CONFIGURAÇÕES DE ERRO DE PREENCHIMENTO
 
   return (
-    <div className="App">
-      <div className="appContainer">
-        <div className="sidebarContainer">
-          <Sidebar step={"step1"}/>
-        </div>
-        <div className="rightAppContainer">
-          <div className="stepContainer">
-            <h1 className="title">Personal info</h1>
-            <p className="stepDescription">Please provide your name, email address, and phone number.</p>
-            <form>
-              <div className={styles.labelInputContainer}>
-                <label className={styles.label}>
-                  <div className={styles.labelErrorContainer}>
-                    <span>Name</span>
-                    <span className={styles.errorMsg} ref={nameErrorMsg}>This field is required</span>
-                  </div>
-                  <input className={styles.input} ref={nameInput} type="text" name="name" id="name" placeholder="e.g. Stephen King" onChange={(e) => setName(e.target.value)} value={name} />
-                </label>
-              </div>
-              <div className={styles.labelInputContainer}>
-                <label className={styles.label}>
-                  <div className={styles.labelErrorContainer}>
-                    <span>Email Address</span>
-                    <span className={styles.errorMsg} ref={invalidEmailMsg}>Invalid email</span>
-                    <span className={styles.errorMsg} ref={emailErrorMsg}>This field is required</span>
-                  </div>
-                  <input className={styles.input} ref={emailInput} type="email" name="email" id="email" placeholder="e.g. stephenking@lorem.com" onChange={(e) => setEmail(e.target.value)} value={email} />
-                </label>
-              </div>
-              <div className={styles.labelInputContainer}>
-                <label className={styles.label}>
-                  <div className={styles.labelErrorContainer}>
-                    <span>Phone Number</span>
-                    <span className={styles.errorMsg} ref={phoneErrorMsg}>This field is required</span>
-                  </div>
-                  <input className={styles.input} ref={phoneInput} type="text" name="phone" id="phone" placeholder="e.g. +1 234 567 890" onChange={(e) => setPhone(e.target.value)} value={phone} />
-                </label>
-              </div>
-            </form>
-          </div>
-          <nav className="navigationContainer">
-            <div></div>
-            <Link onClick={showHideErrorMsg} className="link" to={(name && email && phone && emailRegex.test(email)) ? '/step2' : '/'}><button className="nextBtn">Next Step</button></Link>
-          </nav>
-        </div>
+    <div className="appContainer">
+      <div className="sidebarContainer">
+        <Sidebar step={"step1"}/>
       </div>
+
+      <div className="rightAppContainer">
+        <div className="stepContainer">
+
+          <h1 className="title">Personal info</h1>
+          <p className="stepDescription">Please provide your name, email address, and phone number.</p>
+
+          <form>
+
+            <div className={styles.labelInputContainer}>
+              <label className={styles.label}>
+                <div className={styles.labelErrorContainer}>
+                  <span>Name</span>
+                  <span className={styles.errorMsg} ref={nameErrorMsg}>This field is required</span>
+                </div>
+                <input className={styles.input} ref={nameInput} type="text" name="name" id="name" placeholder="e.g. Stephen King" onChange={(e) => setName(e.target.value)} value={name} />
+              </label>
+            </div>
+
+            <div className={styles.labelInputContainer}>
+              <label className={styles.label}>
+                <div className={styles.labelErrorContainer}>
+                  <span>Email Address</span>
+                  <span className={styles.errorMsg} ref={invalidEmailMsg}>Invalid email</span>
+                  <span className={styles.errorMsg} ref={emailErrorMsg}>This field is required</span>
+                </div>
+                <input className={styles.input} ref={emailInput} type="email" name="email" id="email" placeholder="e.g. stephenking@lorem.com" onChange={(e) => setEmail(e.target.value)} value={email} />
+              </label>
+            </div>
+
+            <div className={styles.labelInputContainer}>
+              <label className={styles.label}>
+                <div className={styles.labelErrorContainer}>
+                  <span>Phone Number</span>
+                  <span className={styles.errorMsg} ref={phoneErrorMsg}>This field is required</span>
+                </div>
+                <input className={styles.input} ref={phoneInput} type="text" name="phone" id="phone" placeholder="e.g. +1 234 567 890" onChange={(e) => setPhone(e.target.value)} value={phone} />
+              </label>
+            </div>
+
+          </form>
+
+        </div>
+        <nav className="navigationContainer">
+          <div></div>
+          <Link onClick={showHideErrorMsg} className="link" to={(name && email && phone && emailRegex.test(email)) ? '/step2' : '/'}><button className="nextBtn">Next Step</button></Link>
+        </nav>
+      </div>
+
     </div>
   );
 };
